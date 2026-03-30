cb42b496a513d34d963d48c89ef428eed710cac7
Leo Mata <>
2026-03-29 22:42:31 -0600

    fix: downgrade strict TypeScript ESLint rules to warnings

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,11 @@
 import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
 
-export default createConfigForNuxt({});
+export default createConfigForNuxt({}).append({
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unsafe-function-type": "warn",
+    "@typescript-eslint/consistent-type-imports": "off",
+    "import/first": "off",
+  },
+});
